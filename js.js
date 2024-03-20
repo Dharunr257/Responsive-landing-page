ab3494f88f17ff6c27a7f49a0c13071f0adcab7d
Dharunr257 <153353402+Dharunr257@users.noreply.github.com>
2024-03-20 12:41:18 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+const togglebtn = document.querySelector(".toggle_btn")
+const togglebtnIcon = document.querySelector(".toggle_btn i")
+const dropdownMenu = document.querySelector(".dropdown-menu")
+
+togglebtn.onclick = function () {
+    dropdownMenu.classList.toggle('open')
+    const isOpen = dropdownMenu.classList.contains('open')
+
+    togglebtnIcon.classList = isOpen?"fa-solid fa-xmark":"fa-solid fa-bars"
+}
